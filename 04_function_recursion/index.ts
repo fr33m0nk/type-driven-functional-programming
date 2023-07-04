faf9b1e99949b884015602eae914cacece599196
fr33m0nk <prashantsinha@protonmail.com>
2023-07-04 23:24:58 +0530

    Fixes auxSumAll

--- 04_function_recursion/index.ts
+++ 04_function_recursion/index.ts
@@ -1,7 +1,7 @@
 type SumAll = (a: number[]) => number
 type SumAllWithAccumulator = (acc: number, xs: number[]) => number
 
-const auxSumAll: SumAllWithAccumulator = (acc, xs) => (xs.length === 0) ? acc : auxSumAll((acc + xs[0]), xs.slice(1))
+const auxSumAll: SumAllWithAccumulator = (acc, xs) => xs[0] ? auxSumAll((acc + xs[0]), xs.slice(1)) : acc
 
 export const sumAll: SumAll = (a) => auxSumAll(0, a)
 
