f9792727d1c144a4cb8639903d12c33d2075ddda
fr33m0nk <prashantsinha@protonmail.com>
2023-07-05 10:58:21 +0530

    Adds LinkedList

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ import {curry} from './03_function_currying'
 import {sumAll} from './04_function_recursion'
 import {Option, Maybe, some, none, isNone, just, nothing, isNothing} from './05_option_and_maybe'
 import {Either, left, right, isLeft} from './06_either'
-
+import {List, isNil, cons, nil, showList} from './07_linked_list'
 /*
 02. Function composition
 */
@@ -91,3 +91,11 @@ const composedEither = compose(
 console.log('06. Uses Either for returned values with composition ', composedEither(0))
 console.log('06. Uses Either for returned values with composition ', composedEither(11))
 console.log('06. Uses Either for returned values with composition ', composedEither(10))
+
+/*
+07. Uses List
+*/
+const emptyList = nil
+console.log('07. My List is ', showList(emptyList))
+const myList: List<number> = cons(1, cons(2, cons(3, nil)))
+console.log('07. My List is ', showList(myList))
