f557c11948be8dc7eb0a62d47eb6ea9450600c15
fr33m0nk <prashantsinha@protonmail.com>
2023-07-06 11:48:26 +0530

    Adds magma, semigroup, monoid

---
+++ 09_magma_semigroup_monoid/index.ts
@@ -0,0 +1,31 @@
+import {List} from '../07_linked_list'
+import {matchListWidened} from '../08_algebraic_data_types'
+
+interface Magma<A> {
+    concat: (x: A, y:A) => A
+}
+
+interface Semigroup<A> extends Magma<A> { }
+
+export const addSemigroup: Semigroup<number> = { concat: (x, y) => x + y }
+export const multiplySemigroup: Semigroup<number> = { concat: (x, y) => x * y }
+export const appendSemigroup: Semigroup<string> = { concat: (x, y) => x.concat(y) }
+
+export const concatAllSemigroup =  <A>(s: Semigroup<A>) => (startWith: A) => (xs: List<A>): A => matchListWidened(
+    () => startWith,
+    (head: A, tail: List<A>) => s.concat(head, concatAllSemigroup(s)(startWith)(tail))
+)(xs)
+
+
+interface Monoid<A> extends Semigroup<A> {
+    empty: A
+}
+
+export const addMonoid: Monoid<number> = { ...addSemigroup, empty: 0 }
+export const multiplyMonoid: Monoid<number> = { ...multiplySemigroup, empty: 1 }
+export const appendMonoid: Monoid<string> = { ...appendSemigroup, empty: '' }
+
+export const concatAllMonoid = <A>(m: Monoid<A>) => (xs: List<A>): A => matchListWidened(
+    () => m.empty,
+    (head: A, tail: List<A>) => m.concat(head, concatAllMonoid(m)(tail))
+)(xs)
--- index.ts
+++ index.ts
@@ -1,14 +1,10 @@
 import {increment, toString, isEven, sum} from './01_functions'
-import {compose} from './02_function_composition'
-import {curry} from './03_function_currying'
-import {sumAll} from './04_function_recursion'
-import {Option, Maybe, some, none, isNone, just, nothing, isNothing} from './05_option_and_maybe'
-import {Either, left, right, isLeft} from './06_either'
-import {List, isNil, cons, nil, showList} from './07_linked_list'
-import {matchOptionWidened, matchEitherWidened, matchListWidened} from './08_algebraic_data_types'
+
 /*
 02. Function composition
 */
+import {compose} from './02_function_composition'
+
 type IncrementThenToString = (n: number) => string
 const incrementThenToString: IncrementThenToString = compose(toString, increment)
 console.log('02. Function composition ', incrementThenToString(10));
@@ -16,6 +12,8 @@ console.log('02. Function composition ', incrementThenToString(10));
 /*
 03. Curried sum
 */
+import {curry} from './03_function_currying'
+
 type CurriedSum = (x: number) =>(y: number) => number
 const curriedSum: CurriedSum = curry(sum)
 console.log('03. Curried sum', curriedSum(10)(-5))
@@ -23,12 +21,16 @@ console.log('03. Curried sum', curriedSum(10)(-5))
 /*
 04. Recursive sum with accumulator
 */
+import {sumAll} from './04_function_recursion'
+
 console.log('04. Recursive sum with accumulator ', sumAll([])) // => 0
 console.log('04. Recursive sum with accumulator ', sumAll([1, 2, 3,4 ,5, 10])) // => 25
 
 /*
 05-1. Uses Option for returned values
 */
+import {Option, some, none, isNone} from './05_option_and_maybe'
+
 type DivideTwoOption = (x: number) => Option<number>
 const divideTwoOption: DivideTwoOption = (x) => x === 0 ? none : some(2/x)
 
@@ -47,6 +49,8 @@ console.log('05-1:Option: ', composedOption(10));
 /*
 05-2. Uses Maybe for returned values
 */
+import {Maybe, just, nothing, isNothing} from './05_option_and_maybe'
+
 type DivideTwoMaybe = (x: number) => Maybe<number>
 const divideTwoMaybe: DivideTwoMaybe = (x) => x === 0 ? nothing : just (2/x)
 
@@ -67,6 +71,7 @@ console.log('05-2:Maybe: ', composedMaybe(10));
 /*
 06. Uses Either for returned values
 */
+import {Either, left, right, isLeft} from './06_either'
 
 type DivideTwoIfEven = (n: number) => Either<string, number>
 const divideTwoIfEven: DivideTwoIfEven = (n: number) => n === 0 ?
@@ -96,6 +101,8 @@ console.log('06. Uses Either for returned values with composition ', composedEit
 /*
 07. Uses List
 */
+import {List, isNil, cons, nil, showList} from './07_linked_list'
+
 const emptyList = nil
 console.log('07. My List is ', showList(emptyList))
 const myList: List<number> = cons(1, cons(2, cons(3, nil)))
@@ -104,38 +111,44 @@ console.log('07. My List is ', showList(myList))
 /*
 08-1. Patter matching on Option
 */
+import {matchOptionWidened} from './08_algebraic_data_types'
+
 const optionResult = (maybeNum: Option<number>) => matchOptionWidened(
     () => 'Value does not exist',
     (a: number) => `value is ${a}`
 )(maybeNum)
 const mayBeNum1 = some(12)
-console.log('08-1. Patter matching on Option ', optionResult(mayBeNum1))
+console.log('08-1. Pattern matching on Option ', optionResult(mayBeNum1))
 const mayBeNum2 = none
-console.log('08-1. Patter matching on Option ', optionResult(mayBeNum2))
+console.log('08-1. Pattern matching on Option ', optionResult(mayBeNum2))
 
 /*
 08-2. Patter matching on Either
 */
+import {matchEitherWidened} from './08_algebraic_data_types'
+
 const eitherResult = (eitherNum: Either<string, number>) => matchEitherWidened(
     (e: string) => `ErrValue: ${e}`,
     (a: number) => `value is: ${a}`
 )(eitherNum)
 const eitherNumOrError1 = left('Error Error Error')
-console.log('08-2. Patter matching on Either ', eitherResult(eitherNumOrError1))
+console.log('08-2. Pattern matching on Either ', eitherResult(eitherNumOrError1))
 const eitherNumOrError2 = right(10)
-console.log('08-2. Patter matching on Either ', eitherResult(eitherNumOrError2))
+console.log('08-2. Pattern matching on Either ', eitherResult(eitherNumOrError2))
 
 /*
 08-3. Patter matching on List
 */
+import {matchListWidened} from './08_algebraic_data_types'
+
 const listResult = (xs: List<number>) => matchListWidened(
     () => 'List is empty'
     , (head: number, tail: List<number>) => `head is ${head}`
 )(xs)
 const myListEmpty = nil
-console.log('08-3. Patter matching on List ', listResult(myListEmpty))
+console.log('08-3. Pattern matching on List ', listResult(myListEmpty))
 const myListv2 = cons(1, cons(2, cons(3, nil)))
-console.log('08-3. Patter matching on List ', listResult(myListv2))
+console.log('08-3. Pattern matching on List ', listResult(myListv2))
 
 /*
 08-4. Patter matching on List using `ts-patten` npm lib
@@ -148,3 +161,34 @@ const listResultv2 = (xs: List<number>) => match(xs)
                                             .exhaustive()
 console.log('08-4. Patter matching on List using `ts-patten` npm lib ', listResultv2(myListEmpty))                                            
 console.log('08-4. Patter matching on List using `ts-patten` npm lib ', listResultv2(myListv2))
+
+
+/*
+09-1. Semigroups (concatAll)
+*/
+import {concatAllSemigroup, addSemigroup, multiplySemigroup, appendSemigroup} from './09_magma_semigroup_monoid'
+
+console.log('09-1. Semigroups (concatAll) using addSemigroup ',
+    concatAllSemigroup(addSemigroup)(0)(cons(2, cons(3, cons(4, nil))))
+)
+console.log('09-1. Semigroups (concatAll) using multiplySemigroup ',
+    concatAllSemigroup(multiplySemigroup)(1)(cons(2, cons(3, cons(4, nil))))
+)
+console.log('09-1. Semigroups (concatAll) using appendSemigroup ',
+    concatAllSemigroup(appendSemigroup)('')(cons('Hello', cons('  ', cons('World!', nil))))
+)
+
+/*
+09-2. Semigroups (concatAll)
+*/
+import {concatAllMonoid, addMonoid, appendMonoid, multiplyMonoid} from './09_magma_semigroup_monoid'
+
+console.log('09-1. Monoid (concatAll) using addMonoid ',
+    concatAllMonoid(addMonoid)(cons(2, cons(3, cons(4, nil))))
+)
+console.log('09-1. Monoid (concatAll) using multiplyMonoid ',
+    concatAllMonoid(multiplyMonoid)(cons(2, cons(3, cons(4, nil))))
+)
+console.log('09-1. Monoid (concatAll) using appendMonoid ',
+    concatAllMonoid(appendMonoid)(cons('Hello', cons('  ', cons('World!', nil))))
+)
