e40c3631675e2fb4a7a8d92bac4fb06066b2c0af
fr33m0nk <prashantsinha@protonmail.com>
2024-04-04 00:21:13 +0530

    Cleanup for Flix

--- 03_function_currying/index.ts
+++
@@ -1,5 +0,0 @@
-type Curry2 = <A, B, C>(
-    f: (x: A, y: B) => C
-) => (x: A) => (y: B) => C
-
-export const curry: Curry2 = f => a => b => f(a, b)
--- 04_function_recursion/index.ts
+++
@@ -1,7 +0,0 @@
-type SumAll = (a: number[]) => number
-type SumAllWithAccumulator = (acc: number, xs: number[]) => number
-
-const auxSumAll: SumAllWithAccumulator = (acc, xs) => xs[0] ? auxSumAll((acc + xs[0]), xs.slice(1)) : acc
-
-export const sumAll: SumAll = (a) => auxSumAll(0, a)
-
